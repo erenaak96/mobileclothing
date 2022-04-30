4fb26d044fff38e1cf7d6b46782ca34b878f7f82
Chelsea <erenakkaya@Chelseas-MacBook-Air.local>
2022-04-30 14:43:28 +0300

    project base

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="" v-if="!isMobile"> Please use mobile device for using this application </div>
+    <Nuxt class="header-spacer main-layout" v-else/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      isMobile: true,
+    };
+  },
+  mounted(){
+    this.getScreen();
+     window.addEventListener('resize', (e)=>{
+        this.getScreen()
+        console.log("resize");
+
+        });
+  },
+  methods:{
+    getScreen(){
+      (window.innerWidth < 767) ? this.isMobile = true : this.isMobile = false;
+        console.log(this.isMobile , window.innerWidth);
+    }
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#181A37',
+        'green': '#74C6B8',
+        'softGray': '#F4F6FA',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray-light': '#d3dce6',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Roboto', 'serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
